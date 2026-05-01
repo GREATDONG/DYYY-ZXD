6eb5b8c84061137e9106af0256cee9b05dd82ce4
GREATDONG <942763262@qq.com>
2026-05-01 22:07:35 +0800

    Simplify Swift Hook modules to minimal implementation

--- DYYYSwiftHookManager.m
+++ DYYYSwiftHookManager.m
@@ -1,16 +1,12 @@
-
 //
 //  DYYYSwiftHookManager.m
 //  DYYY
 //
-//  Swift Hook 安全模式管理器实现
-//
 
 #import "DYYYSwiftHookManager.h"
-#import &lt;objc/runtime.h&gt;
 
 @interface DYYYSwiftHookManager ()
-@property (nonatomic, strong) NSMutableSet&lt;NSString *&gt; *disabledFeatures;
+@property (nonatomic, strong) NSMutableSet *disabledFeatures;
 @end
 
 @implementation DYYYSwiftHookManager
@@ -18,7 +14,7 @@
 + (instancetype)sharedManager {
     static DYYYSwiftHookManager *manager = nil;
     static dispatch_once_t onceToken;
-    dispatch_once(&amp;onceToken, ^{
+    dispatch_once(&onceToken, ^{
         manager = [[DYYYSwiftHookManager alloc] init];
     });
     return manager;
@@ -32,51 +28,31 @@
     return self;
 }
 
-- (BOOL)safeHookSwiftClass:(Class)targetClass
-                  selector:(SEL)selector
-               replacement:(id)replacement
-              featureName:(NSString *)featureName {
-    
-    NSLog(@"[DYYY] SAFE MODE: Feature '%@' marked for hooking (disabled)", featureName);
+- (BOOL)safeHookSwiftClass:(Class)targetClass selector:(SEL)selector replacement:(id)replacement featureName:(NSString *)featureName {
     [self.disabledFeatures addObject:featureName];
     return NO;
 }
 
 - (BOOL)isFeatureAvailable:(NSString *)featureName {
-    if (!featureName || featureName.length == 0) {
-        return NO;
-    }
     return ![self.disabledFeatures containsObject:featureName];
 }
 
 - (void)disableFeature:(NSString *)featureName {
-    if (featureName &amp;&amp; featureName.length &gt; 0) {
+    if (featureName) {
         [self.disabledFeatures addObject:featureName];
-        NSLog(@"[DYYY] SAFE MODE: Feature '%@' manually disabled.", featureName);
     }
 }
 
-- (NSSet&lt;NSString *&gt; *)disabledFeatures {
+- (NSSet *)disabledFeatures {
     return [self.disabledFeatures copy];
 }
 
 - (void)reset {
     [self.disabledFeatures removeAllObjects];
-    NSLog(@"[DYYY] SAFE MODE: Manager reset complete.");
 }
 
 - (void)logStatus {
-    NSLog(@"[DYYY] SAFE MODE: ====================");
-    NSLog(@"[DYYY] SAFE MODE: Status Report");
-    NSLog(@"[DYYY] SAFE MODE: ====================");
-    NSLog(@"[DYYY] SAFE MODE: Disabled Features: %lu", (unsigned long)self.disabledFeatures.count);
-    
-    if (self.disabledFeatures.count &gt; 0) {
-        for (NSString *feature in self.disabledFeatures) {
-            NSLog(@"[DYYY] SAFE MODE:   - %@", feature);
-        }
-    }
-    NSLog(@"[DYYY] SAFE MODE: ====================");
+    NSLog(@"DYYY SAFE MODE: %lu features disabled", (unsigned long)self.disabledFeatures.count);
 }
 
 @end
--- DYYYSwiftHooksSafe.m
+++ DYYYSwiftHooksSafe.m
@@ -1,28 +1,14 @@
-
 //
 //  DYYYSwiftHooksSafe.m
 //  DYYY
 //
-//  Swift Hook 安全模式实现
-//
 
-#import &lt;Foundation/Foundation.h&gt;
-#import &lt;UIKit/UIKit.h&gt;
 #import "DYYYSwiftHookManager.h"
-
-@interface DYYYSwiftHooksSafe : NSObject
-+ (void)setupAllSwiftHooks;
-@end
 
 @implementation DYYYSwiftHooksSafe
 
 + (void)setupAllSwiftHooks {
-    NSLog(@"[DYYY] SAFE MODE: Setting up all Swift Hooks...");
-    
-    dispatch_async(dispatch_get_main_queue(), ^{
-        NSLog(@"[DYYY] SAFE MODE: Swift Hooks initialized (disabled)");
-        [[DYYYSwiftHookManager sharedManager] logStatus];
-    });
+    [[DYYYSwiftHookManager sharedManager] logStatus];
 }
 
 @end
